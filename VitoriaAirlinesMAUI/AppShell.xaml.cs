0980be292b98c8ed3440e1bdc59cbea8131268dd
brcrodrigues96@gmail.com <brcrodrigues96@gmail.com>
2025-07-31 17:02:19 +0100

    Add project files.

---
+++ VitoriaAirlinesMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VitoriaAirlinesMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VitoriaAirlinesMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VitoriaAirlinesMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
